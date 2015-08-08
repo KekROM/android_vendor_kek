06a111c5a84388efe4ee5f23fa4bebb66411bf4f
ezio84-xda <brabus84@gmail.com>
2015-08-08 07:36:22 +0200

    Add Chromium prebuilt and fixup device specific OUT dir [2/2]
    IMPORTANT!!
    Remember to run before building:
    export USE_PREBUILT_CHROMIUM=1
    export ANDROID_FIXUP_COMMON_OUT=true
    
    Updated code for Lollipop by Omnirom and Paranoid
    
    Change-Id: Ic30394e8b432179e2f7e5db55f4f978c7fcaa649

--- config/kek_common.mk
+++ config/kek_common.mk
@@ -4,5 +4,10 @@ KEK_VERSION := alpha
 KEK_BUILD_VERSION := $(KEK_VERSION)-$(shell date +%Y%m%d)-$(BOARD)
 PRODUCT_NAME := $(TARGET_PRODUCT)
 
+# Chromium Prebuilt
+ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
+-include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
+endif
+
 # Set the board version
 CM_BUILD := $(BOARD)
